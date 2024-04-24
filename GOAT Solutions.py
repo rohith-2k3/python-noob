0ffb072b480901b0ba0adc717016b04988eed8f6
rohith-2k3 <tiruveedhularohith4@gmail.com>
2024-04-24 14:14:23 +0530

    prime

---
+++ GOAT Solutions.py
@@ -0,0 +1,16 @@
+# Prime Solution where time complexity is O(√n) for less..
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     if n <= 3:
+#         return True
+#     if n % 2 == 0:
+#         return False
+#     i = 3
+#     while i * i <= n//2:
+#         if n % i == 0:
+#             return False
+#         i += 2
+#     return True
+
+# print(is_prime(4091))
